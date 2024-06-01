694d9ad1b01f8724bd79c5c228865f4c4b32a3f2
gantt42 <gantt42@gmail.com>
2024-05-31 22:01:49 -0400

    Many progress

--- app/src/views/WelcomePage.vue
+++ app/src/views/WelcomePage.vue
@@ -17,7 +17,18 @@ import { ref, onMounted, computed, type StyleValue, type CSSProperties, onUnmoun
 import vuetify from '@/plugins/vuetify';
 import colors from 'vuetify/util/colors'
 
-const bgColorStyle = computed<StyleValue>(() =>
+const styleCol = computed<StyleValue>(() =>
+{
+  let toReturn: StyleValue = {}
+
+  //toReturn["max-width"] = "1500px";
+
+  toReturn["flex"] = "0 1 800px"
+
+  return toReturn
+})
+
+const styleCard = computed<StyleValue>(() =>
 {
   let colors = vuetify.theme.global.current.value.colors
   console.log(colors)
@@ -34,23 +45,24 @@ const bgColorStyle = computed<StyleValue>(() =>
 <template>
 <v-container>
   <v-row
-      class="align-center d-flex ga-0"
-      justify="center"
-      style="height:100%;"
-    >
+    class="align-center d-flex ga-0 rounded-xl"
+    justify="center"
+  >
     <v-col
       v-if="$vuetify.display.smAndUp"
-      style="height:100%; flex: 0 0 20rem;"
+      class="d-flex justify-center align-center"
+      style="height:100%; flex: 0 0 17rem;"
       align="center"
-      class=""
     >
       <v-card
         color="background"
         rounded="xl"
         variant="flat"
-        style="width: 20rem;"
+        style="width: 17rem;"
       >
-        <v-card-text>
+        <v-card-text
+          class="ma-auto"
+        >
           <p class="text-h4 text-center">
             Hi, I'm<br />
             Jacob, a<br />
@@ -64,25 +76,24 @@ const bgColorStyle = computed<StyleValue>(() =>
       class="flex-0-0-100 flex-lg-0-0"
       style="
         flex: 0 0 auto;
-        max-width: calc(24rem * 1.618);
+        max-width: calc(17rem * 1.618);
       "
     >
       <v-card
         rounded="xl"
-        class="text-center"
+        class="text-center pa-0 ma-0"
         variant="flat"
-        :style="bgColorStyle"
+        :style="styleCard"
       >
-        <v-card-text class="text-h4 text-center d-flex ga-3 flex-column">
+        <v-card-text class="text-h4 text-center d-flex pa-0 ma-0 flex-column">
           <p 
-            class="bg-background"
-            rounded="xl"
+            class="bg-background pa-4 rounded-xl"
           >
             I create websites tailored just for you, helping you form connections with your <span class="highlight">online community</span>.
           </p>
+          <div v-if="$vuetify.display.xs" class="pa-2" />
           <p 
-            class="bg-background"
-            rounded="xl"
+            class="bg-background pa-4 rounded-xl"
           >
             Handling the <span class="highlight">technical details</span> is what I do, ensuring your site improves interactions.
           </p>
