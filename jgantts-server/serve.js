602773876a9c49a4e7b2fba2d8bfb00037fc541e
Jacob Gantt <contact@jgantts.com>
2024-12-21 19:30:15 -0500

    Update serve.js

--- jgantts-server/serve.js
+++ jgantts-server/serve.js
@@ -5,11 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const jgantts_com_Path = path.join(__dirname, '../jgantts-com')
+const jgantts_conlangiii_Path = path.join(__dirname, '../conlangiii')
 
 // Serve static files from the dist directory
 app.use('/assets', express.static(path.join(jgantts_com_Path, 'dist', 'assets')));
 app.use(express.static(path.join(jgantts_com_Path, 'PUBLIC')));
 
+app.use('/conlangiii', express.static(path.join(jgantts_conlangiii_Path, 'dist')));
 
 // Serve the index.html file for any other route
 app.get('*', (req, res) => {
