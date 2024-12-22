25ae1fb438f50b1de60ab09e010c23b3469312bc
Jacob Gantt <contact@jgantts.com>
2024-12-21 19:43:48 -0500

    test 500

--- jgantts-server/serve.js
+++ jgantts-server/serve.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 const jgantts_com_Path = path.join(__dirname, '../jgantts-com')
 const jgantts_conlangiii_Path = path.join(__dirname, '../conlangiii')
 
+app.get('*', (req, res) => {
+  res.sendStatus(500)
+});
 
 app.use('/conlangiii/assets', express.static(path.join(jgantts_conlangiii_Path, 'dist', 'assets')));
 app.get('/conlangiii/*', (req, res) => {
