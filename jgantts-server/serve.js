b3f3e1f47706dc453b3e5a86538f6dbb773677a6
Jacob Gantt <contact@jgantts.com>
2024-12-21 19:47:35 -0500

    serve nothing

--- jgantts-server/serve.js
+++ jgantts-server/serve.js
@@ -10,23 +10,6 @@ const jgantts_conlangiii_Path = path.join(__dirname, '../conlangiii')
 app.get('*', (req, res) => {
   res.sendStatus(500)
 });
-
-app.use('/conlangiii/assets', express.static(path.join(jgantts_conlangiii_Path, 'dist', 'assets')));
-app.get('/conlangiii/*', (req, res) => {
-  res.sendStatus(500)
-  res.sendFile(path.join(jgantts_conlangiii_Path, 'dist', 'index.html'));
-});
-
-// Serve static files from the dist directory
-app.use('/assets', express.static(path.join(jgantts_com_Path, 'dist', 'assets')));
-app.use(express.static(path.join(jgantts_com_Path, 'PUBLIC')));
-
-
-// Serve the index.html file for any other route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(jgantts_com_Path, 'dist', 'index.html'));
-});
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
